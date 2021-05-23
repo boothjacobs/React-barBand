663a7d3798b75a87508e3dcac3d8776c0175e827
Sarah <jacobs.b.sarah@gmail.com>
2021-05-22 22:15:23 -0700

    seeds added, database issues persist

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -14,15 +14,10 @@ router.get("", asyncHandler(async (req, res) => {
 
 //route for individual album page
 router.get("/:id", asyncHandler(async (req, res) => {
-    console.log("INSIDE API")
-    const album = await Album.findByPk(req.params.id, {
-        // include: [Comment, Song]
-    });
+    const album = await Album.findByPk(req.params.id);
     if (album) {
         return res.json(album);
-    } else {
-        console.log("find failed")
-    }
+    } 
 }))
 
 module.exports = router;
