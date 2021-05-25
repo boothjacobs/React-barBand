35d5696fb089daeb9975501093a8358c7d4b5447
Sarah <jacobs.b.sarah@gmail.com>
2021-05-24 17:58:30 -0700

    conditional render of edit and delete buttons

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -1,6 +1,10 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import "./comment.css";
 
 const Comment = ({body, user}) => {
+    const sessionUser = useSelector(state => state.session.user);
 
     return (
         // <div id="comment">
@@ -12,6 +16,15 @@ const Comment = ({body, user}) => {
             <div className="content">
                 <p> <b>{user.username}</b> {body}</p>
             </div>
+            { (user.id === sessionUser.id) ?
+                (
+                    <div className="comment-buttons">
+                        <button type="button">Edit</button>
+                        <button type="button">Delete</button>
+                    </div>
+                ) : null
+            }
+
         </div>
 
         //  </div>
