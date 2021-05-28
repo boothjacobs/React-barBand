4628d736e033b54f3fd792c5967bf3d980a2ac79
Sarah <jacobs.b.sarah@gmail.com>
2021-05-28 02:41:18 -0700

    link fix

--- frontend/src/components/Home/AboutBarBand.js
+++ frontend/src/components/Home/AboutBarBand.js
@@ -4,7 +4,7 @@ const AboutBarBand = () => {
     return (
         <div>
             <p>BarBand was built by Sarah Booth Jacobs using React, Redux, Express, and Sequelize.</p>
-            <Link to="https://www.linkedin.com/in/sarah-jacobs-53433923/">LinkedIn</Link> <Link to="https://github.com/boothjacobs">Github</Link>
+            <a href="https://www.linkedin.com/in/sarah-jacobs-53433923/">LinkedIn</a> <a href="https://github.com/boothjacobs">Github</a>
         </div>
     );
 }
